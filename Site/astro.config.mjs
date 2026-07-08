2e7f5775a068cc8acc02d24bae706662fdd9a99e
Bladeswillfall <Bladeswillfall@gmail.com>
2026-07-08 11:45:22 +0100

    Update Starlight demo pages

--- Site/astro.config.mjs
+++ Site/astro.config.mjs
@@ -18,7 +18,7 @@ import siteConfig from './site.config.mjs';
 export default defineConfig({
   site: siteConfig.site,
   integrations: [
-    mdx()
+    mdx(),
     starlight({
       title: siteConfig.title,
       description: siteConfig.description,
